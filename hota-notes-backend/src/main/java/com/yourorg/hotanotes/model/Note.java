b291bf861d070f16f035b6604c49bb5deaf34ca1
galiannm <galiannm@tcd.ie>
2025-06-16 13:39:53 +0700

    Added category into Notes

--- hota-notes-backend/src/main/java/com/yourorg/hotanotes/model/Note.java
+++ hota-notes-backend/src/main/java/com/yourorg/hotanotes/model/Note.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -32,6 +35,10 @@ public class Note {
 
     @Lob
     private String content;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=true)
+    @JoinColumn(name="category_id")
+    private Category category;
 
     @Column(name = "created_at", updatable = false)
     private Instant createdAt;
