90be6b29fb3986318ec1eecf9b68599a547965a8
galiannm <galiannm@tcd.ie>
2025-06-16 22:12:47 +0700

    Added Tag service and controller

---
+++ hota-notes-backend/src/main/java/com/yourorg/hotanotes/controller/TagController.java
@@ -0,0 +1,54 @@
+package com.yourorg.hotanotes.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yourorg.hotanotes.model.Tag;
+import com.yourorg.hotanotes.service.TagService;
+
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+    private final TagService service;
+
+    public TagController(TagService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Tag> all() {
+        return service.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Tag one(@PathVariable Long id) {
+        return service.findById(id);
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Tag create(@RequestBody Tag tag) {
+        return service.save(tag);
+    }
+
+    @PutMapping("/{id}")
+    public Tag update(@PathVariable Long id, @RequestBody Tag tag) {
+        return service.update(id, tag);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
