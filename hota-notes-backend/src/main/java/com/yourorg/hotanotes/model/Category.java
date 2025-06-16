51b5be62d97283b7b154deac8c0ec264c6c03158
galiannm <galiannm@tcd.ie>
2025-06-16 13:01:29 +0700

    Added Category and its repo and service

---
+++ hota-notes-backend/src/main/java/com/yourorg/hotanotes/model/Category.java
@@ -0,0 +1,59 @@
+package com.yourorg.hotanotes.model;
+
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+    // defiines the relationship with notes: category gets deleted -> notes get deleted ('cascade')
+    @OneToMany(mappedBy="category", cascade=CascadeType.ALL, orphanRemoval=true)
+    @Builder.Default
+    private List<Note> notes = new ArrayList<>();
+
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+}
